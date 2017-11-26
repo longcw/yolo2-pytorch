8b4f56a7f909958c047fdfdc55cbb9dc7c7de9e7
Trent Weiss <ttw2xk@virginia.edu>
2017-11-26 15:07:31 -0500

    use proper gpu type

--- make.sh
+++ make.sh
@@ -8,14 +8,14 @@ cd ../
 
 cd layers/reorg/src
 echo "Compiling reorg layer kernels by nvcc..."
-nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_30
 cd ../
 python build.py
 cd ../
 
 cd roi_pooling/src/cuda
 echo "Compiling roi_pooling kernels by nvcc..."
-nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_30
 cd ../../
 python build.py
 cd ../
