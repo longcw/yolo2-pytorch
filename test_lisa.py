e964acf112495ed4644e20766ab2ccf59d2ea219
Trent <ttw2xk@virginia.edu>
2017-12-02 22:57:41 +0000

    test with new weights

--- test_lisa.py
+++ test_lisa.py
@@ -25,13 +25,13 @@ def preprocess(fname):
 output_dir = 'test_results'
 
 max_per_image = 300
-thresh = 0.2
-vis = True
+thresh = 0.5
+vis = False
 # ------------
-
+cuda = True
 
 def test_net(net, imdb, max_per_image=300, thresh=0.5, vis=False):
-    num_images = imdb.num_images
+    num_images = 100
 
     # all detections are collected into:
     #    all_boxes[cls][image] = N x 5 array of detections in
@@ -47,7 +47,7 @@ def test_net(net, imdb, max_per_image=300, thresh=0.5, vis=False):
         print('testing on the ' + str(i+1) + 'th batch')
         batch = imdb.next_batch()
         ori_im = batch['origin_im'][0]
-        im_data = net_utils.np_to_variable(batch['images'], is_cuda=False, volatile=True).permute(0, 3, 1, 2)
+        im_data = net_utils.np_to_variable(batch['images'], is_cuda=cuda, volatile=True).permute(0, 3, 1, 2)
         print("got im_data")
         _t['im_detect'].tic()
 	print(im_data.size())
@@ -95,6 +95,12 @@ def test_net(net, imdb, max_per_image=300, thresh=0.5, vis=False):
                 im2show = cv2.resize(im2show, (int(1000. * float(im2show.shape[1]) / im2show.shape[0]), 1000))
             cv2.imshow('test', im2show)
             cv2.waitKey(0)
+        else:
+            im2show = yolo_utils.draw_detection(ori_im, bboxes, scores, cls_inds, cfg, thr=0.1)
+            if im2show.shape[0] > 1100:
+                im2show = cv2.resize(im2show, (int(1000. * float(im2show.shape[1]) / im2show.shape[0]), 1000))            
+            output_path=os.path.join('output_images','test_image_'+str(i)+'.jpg')
+            cv2.imwrite(output_path,im2show)
 
     with open(det_file, 'wb') as f:
         cPickle.dump(all_boxes, f, cPickle.HIGHEST_PROTOCOL)
@@ -111,7 +117,8 @@ if __name__ == '__main__':
     net = Darknet19()
     net_utils.load_net(os.path.join('lisa_models', 'yolo_lisa_with_synthetic_data_99.h5'), net)
     print('load net succ...')
-   # net.cuda()
+    if cuda:
+        net.cuda()
     net.eval()
 
     test_net(net, imdb, max_per_image, thresh, vis)
